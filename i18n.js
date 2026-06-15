e39157d654bd23cff8d566a66ed6415c38fd4ff6
pedrolucasdourado <pedrolucasdourado@hotmail.com>
2026-06-15 11:40:53 -0300

    feat: detect system language and add language switcher

--- i18n.js
+++ i18n.js
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import { getSystemLanguage, DEFAULT_LANGUAGE } from './utils/language';
 
 // load the Language .json files
 import en from './locales/en.json'
@@ -8,8 +9,8 @@ import pt from './locales/pt.json'
 // I18N Set up
 i18n.use(initReactI18next)
     .init({
-        lng: 'en',
-        fallbackLng: 'en',
+        lng: getSystemLanguage(),
+        fallbackLng: DEFAULT_LANGUAGE,
         compatibilityJSON: 'v3',
         interpolation: {
             escapeValue: false,
--- styles/components/LangChanger.styles.js
+++ styles/components/LangChanger.styles.js
@@ -3,13 +3,27 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     position: "absolute",
-    top: 10,
-    right: 10,
+    top: 48,
+    right: 16,
+    zIndex: 100,
+    elevation: 8,
+  },
+  button: {
     flexDirection: "row",
     alignItems: "center",
+    gap: 6,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 24,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    shadowColor: "#000000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.18,
+    shadowRadius: 4,
   },
   label: {
-    fontSize: 16,
-    marginRight: 5,
+    color: "#14213D",
+    fontSize: 13,
+    fontWeight: "700",
   },
 });
